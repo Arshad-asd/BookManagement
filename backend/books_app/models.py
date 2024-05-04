38f9b7b6617d3f1f8520e1f4c3394be14535a413
Arshad <arshaarshad21@gmail.com>
2024-05-04 21:07:39 +0530

    Books model created

--- backend/books_app/models.py
+++ backend/books_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    authors = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    description = models.TextField(blank=True)
+    document = models.FileField(
+        upload_to='books/documents/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ReadingList(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reading_lists')
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(
+        'Book', related_name='reading_lists', through='ReadingListItem')
+
+    def __str__(self):
+        return f"{self.user}'s Reading List - {self.name}"
+
+
+class ReadingListItem(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    reading_list = models.ForeignKey(ReadingList, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('book', 'reading_list')
+
+    def __str__(self):
+        return f"{self.book} in {self.reading_list} at position {self.order}"
